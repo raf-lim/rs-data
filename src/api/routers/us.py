c72f1cec79d1fcbe5f1028e7944c5597ef9d7bff
raf-lim <105244879+raf-lim@users.noreply.github.com>
2023-11-13 15:43:08 +0100

    27 change table schema for metrics meta (#29)
    * Change schema of
    
    * Change naming of metrics from name into code.
    
    * Add endpoints with metrics' metadata.

--- src/api/routers/us.py
+++ src/api/routers/us.py
@@ -1,3 +1,4 @@
+from typing import Any
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from db.base import get_db
@@ -10,39 +11,41 @@ router_us = APIRouter(prefix="/us", tags=["us"])
 @router_us.get("/metrics")
 async def get_metrics_data_endpoints(
     db: Session = Depends(get_db),
-    ) -> dict[str, str]:
+    ) -> dict[str, dict[str, str]]:
     return us.create_endpoints_to_metrics_data(db)
 
 
-@router_us.get("/metric/{metric}")
-async def get_metric_data_and_statistics(
-    metric: str, limit: int = 0, db: Session = Depends(get_db),
-    ) -> dict[str, dict[str, dict[str, float | None]]]:
+@router_us.get("/metric/{metric_code}")
+async def get_metric_all_data(
+    metric_code: str, limit: int = 0, db: Session = Depends(get_db),
+    ) -> dict[str, dict[str, str] | dict[str, dict[str, float | None]]]:
     
-    return us.get_metric_all_info_from_db(metric, limit, db)
+    return us.get_metric_all_info_from_db(metric_code, limit, db)
 
 
 @router_us.get("/metric/{metric_code}/metadata")
 async def get_metric_metadata(
-    metric_code, db: Session = Depends(get_db),
-    ):
+    metric_code,
+    db: Session = Depends(get_db),
+    ) -> dict[str, Any]:
+    metadata = us.get_metric_metadata_from_db(metric_code, db)
 
-    return us.get_metric_metadata_from_db(metric_code, db)
+    return us.add_metric_endpoint_url_to_metadata(metadata)
 
 
-@router_us.get("/metric/{metric}/data")
+@router_us.get("/metric/{metric_code}/data")
 async def get_metric_data(
-    metric: str, limit: int = 0, db: Session = Depends(get_db)
+    metric_code: str, limit: int = 0, db: Session = Depends(get_db)
     ) -> dict[str, dict[str, float | None]]:
     
-    return us.get_metric_data_from_db(metric, limit, db)
+    return us.get_metric_data_from_db(metric_code, limit, db)
 
 
-@router_us.get("/metric/{metric}/stats")
+@router_us.get("/metric/{metric_code}/stats")
 async def get_metric_statistics(
-    metric: str, db: Session = Depends(get_db),
+    metric_code: str, db: Session = Depends(get_db),
     ) -> dict[str, dict[str, float | None]]:
     
-    return us.get_metric_statistics_from_db(metric, db)
+    return us.get_metric_statistics_from_db(metric_code, db)
 
 
