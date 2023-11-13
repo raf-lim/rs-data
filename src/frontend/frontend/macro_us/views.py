2ac77892c1ad940769e1bdec432969fc07fc70ba
raf-lim <raf.sukiennik@gmail.com>
2023-11-13 17:44:39 +0100

    Develop us metrics view.

--- src/frontend/frontend/macro_us/views.py
+++ src/frontend/frontend/macro_us/views.py
@@ -16,24 +16,35 @@ from frontend.libs import stylers
 
 class MacroUsMetrics(LoginRequiredMixin, View):
     # imgs_dir = os.path.join(STATIC_ROOT, "img_us")
-    API_BASE_URL = os.getenv("API_BASE_URL", default="https://rs-data-api.up.railway.app")
+    API_BASE_URL = os.getenv("API_BASE_URL")
 
     def get(self, request):
-        url = os.path.join(self.API_BASE_URL, "us/metric/housing/all")
+        url = os.path.join(self.API_BASE_URL, "us/metrics")
         response = requests.get(url)
         response.raise_for_status()
 
-        raw_data = response.json()
+        metrics_metadata = response.json()
 
-        data = pd.DataFrame(raw_data["data"])[-6:]
-        stats = pd.DataFrame(raw_data["statistics"])
+        metrics_tables = {}
 
-        res = pd.concat([data, stats]).transpose()
+        for metric_code, meta in metrics_metadata.items():
+            metric_all_data_url = os.path.join(self.API_BASE_URL, "us/metric", metric_code)
+            response = requests.get(metric_all_data_url)
+            response.raise_for_status()
 
-        res = stylers.MetricTableStyler(res).style_table_index_with_change_stats()
+            metric_all_data = response.json()
+
+            data = pd.DataFrame(metric_all_data["data"])[-6:]
+            stats = pd.DataFrame(metric_all_data["statistics"])
+
+            data_with_stats = pd.concat([data, stats]).transpose()
+
+            res = stylers.MetricTableStyler(data_with_stats).style_table_index_with_change_stats()
+
+            metrics_tables[metric_code] = res.to_html()
 
 
-        context = {"housing": res.to_html()}
+        context = {"metrics_tables": metrics_tables}
 
         return render(request, "macro_us/us_metrics.html", context)
 
