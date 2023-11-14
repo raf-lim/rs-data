2d39b90f2809065419ac9d30c7dfe421aeb861a8
raf-lim <raf.sukiennik@gmail.com>
2023-11-14 22:54:58 +0100

    Convert EU metric interface from Protocol into pytdantic BaseModel.

--- src/updater/updaters/eu/interfaces.py
+++ src/updater/updaters/eu/interfaces.py
@@ -1,6 +1,7 @@
-from typing import Protocol
+from pydantic import BaseModel
 
 
-class EsiMetric(Protocol):
+class EsiMetric(BaseModel):
+    """Represents interface of EU ESI metric"""
     code: str
     name: str
