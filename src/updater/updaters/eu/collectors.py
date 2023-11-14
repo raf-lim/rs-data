34f410b6f507a7e11b28b15cb2655be71832f805
raf-lim <raf.sukiennik@gmail.com>
2023-11-14 19:49:20 +0100

    Add collectors functions.

---
+++ src/updater/updaters/eu/collectors.py
@@ -0,0 +1,55 @@
+import os
+import logging
+from typing import Protocol, Callable, Optional, Any
+import requests
+from requests import HTTPError
+import pandas as pd
+
+from updaters.eu import metrics, countries
+from updaters.libs import statistics
+from updaters.eu.interfaces import EsiMetric
+
+logging.basicConfig(level=logging.INFO)
+
+
+class EuMetric(Protocol):
+    code: str
+    name: str
+    frequency: Optional[str]
+
+
+def compile_endpoint_url(
+        base_url: str, country_code: str, metric_code: str,
+        ) -> str:
+    """Compile endpoint url to get metric data for a country."""
+    
+    return os.path.join(
+            base_url, f"?geo={country_code}&indic={metric_code}&s_adj=SA",
+            )
+
+
+def get_data(url: str) -> pd.DataFrame:
+    """Get Eurostat data for a metric of a country."""
+    response = requests.get(url, timeout=None)
+    response.raise_for_status()
+    return response.json()
+
+
+JSON = dict[str, str | dict[str | Any]]
+
+
+def parse_country_metric_data(data: JSON):
+    """Parse data received from Eurostat API."""
+    dates = pd.Series(
+        data['dimension']['time']['category']['index'],
+        dtype='object',
+        )
+    dates.index.name = 'date'
+    dates = dates.to_frame().reset_index().drop(columns=[0])
+
+    values = pd.Series(data['value'], dtype=float)
+    values.index = values.index.astype(int)
+    values = values.to_frame()
+    
+    return dates.join(values).set_index('date').sort_index()
+    
