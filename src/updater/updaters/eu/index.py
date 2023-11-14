cd8389c3b4dad5f7ad9951df6514569d462961c4
raf-lim <raf.sukiennik@gmail.com>
2023-11-14 22:58:22 +0100

    Add statistic computing to eu_main.

--- src/updater/updaters/eu/index.py
+++ src/updater/updaters/eu/index.py
@@ -1,18 +1,19 @@
-import os
+from os import getenv
 import logging
 from requests import HTTPError
 import pandas as pd
 from db.base import engine
-
+from updaters.libs import cleaners, statistics
 from updaters.eu import countries, metrics, collectors
 
 
 def main_eu():
     """Main function for EU updater."""
-    BASE_URL = os.getenv("EUROSTAT_BASE_URL")
-    MONTHS_LIMIT = int(os.getenv("EU_DATA_MONTHS_LIMIT"))
+    
+    BASE_URL = getenv("EUROSTAT_BASE_URL")
+    MONTHS_LIMIT = int(getenv("EU_DATA_MONTHS_LIMIT"))
 
-    countries_metric_tables: dict[str, pd.DataFrame] = {}
+    # countries_metrics_data: dict[str, pd.DataFrame] = {}
     for metric in metrics.esi_metrics:
         
         countries_metric: list[pd.DataFrame] = []
@@ -29,32 +30,58 @@ def main_eu():
                     )
                 continue
 
-            country_metric = (
+            country_metric_data = (
                 collectors.parse_country_metric_data(data)[-MONTHS_LIMIT:]
                 )
-            country_metric.rename(
+            country_metric_data.rename(
                 columns={
-                    country_metric.columns[0]: f"{country_code}-{metric.name}",
+                    country_metric_data.columns[0]: 
+                    f"{country_code}-{metric.name}",
                     },
                 inplace=True,
                 )
-            countries_metric.append(country_metric)
+            countries_metric.append(country_metric_data)
 
-        countries_metric_table = pd.concat(countries_metric, axis=1).sort_index()
-        countries_metric_tables[metric.name] = countries_metric_table 
+        countries_metric_data_table = (
+            pd.concat(countries_metric, axis=1).sort_index()
+            )
+        # Clean constituents series recently not reported.
+        # and convert into percentage change series if relevant.  
+        countries_metric_data_table = cleaners.remove_longer_not_reported(
+            data=countries_metric_data_table, last_not_reported=6,
+            )
+        # countries_metrics_data[metric.name] = countries_metric_data_table
 
+        stats = pd.DataFrame()
+        for country in countries_metric_data_table.columns:
+            data = countries_metric_data_table[country]
+            stats_data = statistics.compute_statistics_difference(
+                data, year_readings_number=12,
+                )
+            stats_data.name = country
+            stats = pd.concat([stats, stats_data], axis=1)
 
         # Save metric tables in database
         with engine.connect() as conn:
-            for metric_name, metric_table in countries_metric_tables.items():
-                try:
-                    metric_table.to_sql(
-                        name=f"eu_metric_{metric_name.lower()}_data",
-                        con=conn,
-                        if_exists="replace",
-                        index=True,
-                        )
-                except KeyError as e:
-                    logging.warning(f"KeyError: {e}")
             
+            try:
+                countries_metric_data_table.to_sql(
+                    name=f"eu_metric_{metric.name.lower()}_data",
+                    con=conn,
+                    if_exists="replace",
+                    index=True,
+                    )
+            except KeyError as e:
+                logging.warning(f"KeyError: {e}")
+            
+            try:
+                stats.to_sql(
+                    name=f"eu_metric_{metric.name.lower()}_stats",
+                    con=conn,
+                    if_exists="replace",
+                    index=True,
+                    )
+            except KeyError as e:
+                logging.warning(f"KeyError: {e}")
+
             conn.commit()
