f7878b9f9297bdfd099c5b4c4113dc568edf128c
raf-lim <raf.sukiennik@gmail.com>
2023-11-16 12:40:38 +0100

    Add countries operations.

--- src/api/operations/eu.py
+++ src/api/operations/eu.py
@@ -9,10 +9,10 @@ import numpy as np
 THIS_API_BASE_URL = os.getenv("THIS_API_BASE_URL")
 
 
-def extract_metrics_names_from_db(
+def extract_metrics_codes_from_db(
         db: Session,
     ) -> Iterable[str]:
-    """Extract EU metrics' names from tables' names in database"""
+    """Extract EU metrics' codes from tables' names in database"""
     tables = db.scalars(
         text(f"""
              SELECT table_name
@@ -27,6 +27,9 @@ def extract_metrics_names_from_db(
                  }).all()
     
     return [table_name.split("_")[-2] for table_name in tables]
+
+
+
 
 
 def create_metric_metadata(
@@ -50,7 +53,7 @@ MetadataCreatorFn = Callable[[str, str], dict[str, str]]
 
 def create_all_metrics_metadata(
         db: Session,
-        metrics_getter: MetricsCodesGetterFn = extract_metrics_names_from_db,
+        metrics_getter: MetricsCodesGetterFn = extract_metrics_codes_from_db,
         metadata_creator: MetadataCreatorFn = create_metric_metadata,
         ) -> dict[str, dict[str, str]]:
     metrics_codes = metrics_getter(db)
@@ -111,3 +114,76 @@ def get_metric_all_info(
     result.update({"statistics": stats})
     
     return result
+
+
+def extract_countries_codes_from_db(
+        db: Session,
+    ) -> Iterable[str]:
+    """Extract EU countries' codes from tables' names in database"""
+    tables = db.scalars(
+        text(f"""
+             SELECT table_name
+             FROM information_schema.tables
+             WHERE table_schema = :table_schema
+             AND table_name LIKE :table_name_prefix
+             AND table_name LIKE :table_name_suffix
+             """), {
+                 "table_schema": "public",
+                 "table_name_prefix": "eu_country%",
+                 "table_name_suffix": "%data"
+                 }).all()
+    
+    return (
+        table_name
+        .lstrip("eu")
+        .lstrip("_")
+        .lstrip("country")
+        .lstrip("_")
+        .rstrip("data")
+        .rstrip("_")
+          for table_name in tables
+        )
+
+
+def get_country_data_from_db(
+        country_code: str,
+        limit: Optional[int],
+        db: Session
+    ) -> dict[str, dict[str, float | None]]:
+    """
+    Get country's data from database (all constituents).
+    """
+    data = pd.read_sql_table(
+        table_name=f"eu_country_{country_code}_data",
+        con=db.connection(),
+        index_col="date"
+    )[-limit:]
+    data = data.replace(to_replace=np.NaN, value=None)
+
+    return data.to_dict()
+
+
+def get_country_statistics_from_db(
+        country_code: str,
+        db: Session,
+        metrics_getter: MetricsCodesGetterFn = extract_metrics_codes_from_db,
+    ) -> dict[str, dict[str, float | None]]:
+    """
+    Get country's stats from database (all constituents).
+    """
+    metrics_stats: dict[str, dict[str, float | None]] = {}
+    metrics_codes = metrics_getter(db)
+    for metric_code in metrics_codes:
+        metric_name: str = f"{country_code}-{metric_code}".upper()
+        metric_stats = db.scalars(
+            text(f"""
+                 SELECT (index, "{metric_name}")
+                 FROM eu_metric_{metric_code.lower()}_stats
+                 """)
+            ).all()
+        metrics_stats[metric_name] = {row[0]: row[1] for row in metric_stats}
+
+    return metrics_stats
+
+# TODO: All info for country
+# TODO: Countries metadata (list of countries)
