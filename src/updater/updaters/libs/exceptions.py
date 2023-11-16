bc636df40b238c61abb5399b70b12bcf1115d8cd
raf-lim <raf.sukiennik@gmail.com>
2023-11-17 00:27:46 +0100

    Move updater exceptions module to updater libs.

---
+++ src/updater/updaters/libs/exceptions.py
@@ -0,0 +1,10 @@
+class MissingFredApiKeyException(Exception):
+    """Raised when FRED API key is not set"""
+
+
+class FredApiNoObservationsDataException(Exception):
+    """Raised when FRED API sends invalid data"""
+
+
+class NoTableFoundException(Exception):
+    """Raised when table not found in database"""
